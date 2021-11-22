972e2b106970d7712d6ac4d6f5b964af5b0b8dab
Wlxty <kmosc97@gmail.com>
2021-11-22 13:39:29 +0100

    Reorganizing code

--- Data/Fibbonaci.go
+++ Data/Fibbonaci.go
@@ -1,17 +1,17 @@
 package Data
 
 func Fibbonaci(size int) []int{
-	if size < 0{
+	if size <= 0{
 		return nil
 	}else{
 		array := []int{0}
-		for num := 0; num < size - 1; num++ {
-			if num == 0 {
-				array = append(array, 1)
-			}else {
-				newNum := array[num] + array[num-1]
-				array = append(array, newNum)
-			}
+
+		if size > 1 {
+			array = append(array, 1)
+		}
+		for num := 1; num < size - 1; num++ {
+			newNum := array[num] + array[num-1]
+			array = append(array, newNum)
 		}
 		return array
 	}
