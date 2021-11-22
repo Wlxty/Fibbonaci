5ba262179c28ef7a6c403c462f7e5cb2f832f4a6
Wlxty <kmosc97@gmail.com>
2021-11-22 12:32:20 +0100

    re-organizing code

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
-import("fmt")
-
-func Fibbonaci(size int) []int{
-	if size < 1{
-		return nil
-	}else{
-		array := []int{1}
-		for num := 0; num < size; num++ {
-			if num == 0 {
-				array = append(array, 1)
-			}else {
-				newNum := array[num] + array[num-1]
-				array = append(array, newNum)
-			}
-		}
-		return array
-	}
-}
+import(
+	"fmt"
+	"Fibbonaci/Data"
+)
 
 func main(){
-	fmt.Println(Fibbonaci(6))
+	fmt.Println(Data.Fibbonaci(6))
 }
