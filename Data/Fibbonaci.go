9559b347fbb8ee13446d2e13b165abfdcca99a82
Wlxty <kmosc97@gmail.com>
2021-11-22 13:28:45 +0100

    fixing fibbonaci sequence

--- Data/Fibbonaci.go
+++ Data/Fibbonaci.go
@@ -1,11 +1,11 @@
 package Data
 
 func Fibbonaci(size int) []int{
-	if size < 1{
+	if size < 0{
 		return nil
 	}else{
-		array := []int{1}
-		for num := 0; num < size; num++ {
+		array := []int{0}
+		for num := 0; num < size - 1; num++ {
 			if num == 0 {
 				array = append(array, 1)
 			}else {
